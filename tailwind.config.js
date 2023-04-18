a62b289af8cfd12d8b98fcb3aed8245057806603
tommy-holmes <thg.holmes96@gmail.com>
2023-04-18 19:37:37 +0800

    Changes nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,6 @@ module.exports = {
   content: ["./index.{html,js}"],
   theme: {
     extend: {
-      typography: {
-        quoteless: {
-          css: {
-            'blockquote p:first-of-type::before': { content: 'none' },
-            'blockquote p:first-of-type::after': { content: 'none' },
-          },
-        },
-      },
       colors: {
         'carbon': '#151517',
         'egg': '#f4f4f4',
