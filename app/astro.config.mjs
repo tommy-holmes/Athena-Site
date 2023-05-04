2cf1087f2de4574c62eced6c702cc3c3cbdb0e9a
tommy-holmes <thg.holmes96@gmail.com>
2023-05-04 19:39:10 +0700

    Adds to astro config GH Pages

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tommy-holmes.github.io',
+  base: '/athena-site',
   integrations: [
     tailwind(),
     sanity({
