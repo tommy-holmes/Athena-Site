a5986e0274b82dfb10c029c43cadd46f6a98b89a
tommy-holmes <thg.holmes96@gmail.com>
2023-05-04 19:40:14 +0700

    Check

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tommy-holmes.github.io',
-  base: '/athena-site',
+  base: '/Athena-Site',
   integrations: [
     tailwind(),
     sanity({
